adbdf05e7bce97587362431959e778115e70ddb8
corecasttf <corecasttf@protonmail.com>
2020-11-19 22:43:54 -0500

    Added map score section and updated scoreboard.

--- scoreboard/script.js
+++ scoreboard/script.js
@@ -7,13 +7,7 @@ for (let scoreElem of teamElements) {
 	});
 }
 
-function swapTeams() {
-	let firstName = teamElements[0].textContent;
-	teamElements[0].textContent = teamElements[1].textContent;
-	teamElements[1].textContent = firstName;
-}
-
-// Score change
+// Score change.
 let scoreElements = document.getElementsByClassName('score');
 
 for (let scoreElem of scoreElements) {
@@ -21,7 +15,70 @@ for (let scoreElem of scoreElements) {
 		scoreElem.textContent = parseInt(scoreElem.textContent) + 1;
 	});
 	scoreElem.addEventListener('contextmenu', (e) => {
-		e.preventDefault();
 		scoreElem.textContent = parseInt(scoreElem.textContent) - 1;
+		e.preventDefault();
 	});
+}
+
+// Map scrores.
+let mapNameElements = document.getElementsByClassName('map-name');
+let mapScoreElements = document.getElementsByClassName('map-score');
+let mapTeamScoreElements = document.getElementsByClassName('map-team-score');
+
+for (let i = 0; i < mapNameElements.length; i++) {
+	let mapNameElem = mapNameElements[i];
+
+	mapNameElem.addEventListener('click', (e) => {
+		let mapScoreElem = mapScoreElements[i];
+
+		if (mapScoreElem.classList.contains('show')) {
+			mapScoreElem.classList.remove('show');
+		} else if (mapNameElem.classList.contains('highlighted')) {
+			mapScoreElem.classList.add('show');
+			mapNameElem.classList.remove('highlighted');
+		} else {
+			mapNameElem.classList.add('highlighted');
+		}
+	});
+
+	mapNameElem.addEventListener('contextmenu', (e) => {
+		if (mapNameElem.classList.contains('strike-through')) {
+			mapNameElem.classList.remove('strike-through');
+		} else {
+			mapNameElem.classList.add('strike-through');
+		}
+		e.preventDefault();
+	});
+}
+
+for (let mapTeamScoreElem of mapTeamScoreElements) {
+	mapTeamScoreElem.addEventListener('click', (e) => {
+		mapTeamScoreElem.textContent = parseInt(mapTeamScoreElem.textContent) + 1;
+	});
+	mapTeamScoreElem.addEventListener('contextmenu', (e) => {
+		e.preventDefault();
+		mapTeamScoreElem.textContent = parseInt(mapTeamScoreElem.textContent) - 1;
+	});
+}
+
+
+
+
+
+
+
+
+
+function swapTeams() {
+	// Team names.
+	let firstName = teamElements[0].textContent;
+	teamElements[0].textContent = teamElements[1].textContent;
+	teamElements[1].textContent = firstName;
+
+	// Map scores.
+	for (let i = 0; i < mapTeamScoreElements.length; i += 2) {
+		let tempScore = mapTeamScoreElements[i].textContent;
+		mapTeamScoreElements[i].textContent = mapTeamScoreElements[i + 1].textContent;
+		mapTeamScoreElements[i + 1].textContent = tempScore;
+	}
 }
